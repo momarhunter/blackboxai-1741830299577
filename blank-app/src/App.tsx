289d5d68745cd1406c88b3fe968aa4a7668ca42d
momarhunter <kirammomar2@gmail.com>
2025-03-13 09:46:53 +0800

    Add blank-app/src/App.tsx 1741830411253

---
+++ blank-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { SplineSceneBasic } from './components/demo';
+
+function App() {
+  return (
+    <>
+      <div><SplineSceneBasic/></div>
+    </>
+  );
+}
+
+export default App;
